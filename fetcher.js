293e88fa714fd39cfad11e10e6ae76600c9e2b4f
MrChan <mrkcwayne@gmail.com>
2023-06-17 00:19:55 -0600

    added a function that will take in things, added 2 const that will require the request and file system

--- fetcher.js
+++ fetcher.js
@@ -6,3 +6,20 @@ It should take two command line arguments:
 3. It should download the resource at the URL to the local path on your machine.
 4. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
 */
+const fs = require('fs');
+const { get } = require('request');
+
+const [url, filePath] = process.argv.slice(2);
+
+const downloadResource = (url, filePath) => {
+  get(url, (error, response, body) => {
+    if (error) {
+      console.log('Error:', error);
+      return;
+    }
+
+
+  });
+};
+
+downloadResource(url, filePath);
