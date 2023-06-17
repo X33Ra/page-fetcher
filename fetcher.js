a42425f4ed48a04f95204c8c47ed38c89431fc29
MrChan <mrkcwayne@gmail.com>
2023-06-17 00:13:55 -0600

    added a Rule to fetcher

---
+++ fetcher.js
@@ -0,0 +1,8 @@
+/**
+ * Rules:
+It should take two command line arguments:
+1. a URL
+2. a local file path
+3. It should download the resource at the URL to the local path on your machine.
+4. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
+*/
